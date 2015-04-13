2b1fa6ea0232fae0355e9d6acd23e11bf0877ff5
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2015-04-13 10:52:34 +0200

    added test assertions, removed printlns.

--- src/test/java/org/aksw/cubeqa/detector/HalfInfiniteIntervalDetectorTest.java
+++ src/test/java/org/aksw/cubeqa/detector/HalfInfiniteIntervalDetectorTest.java
@@ -1,20 +1,23 @@
 package org.aksw.cubeqa.detector;
 
+import static org.junit.Assert.*;
 import java.util.Set;
 import org.aksw.cubeqa.Cube;
+import org.aksw.cubeqa.restriction.Restriction;
 import org.aksw.cubeqa.template.CubeTemplateFragment;
 import org.junit.Test;
 
 public class HalfInfiniteIntervalDetectorTest
 {
-
 	@Test public void testDetect()
 	{
 		Cube cube = Cube.FINLAND_AID;
 		Set<CubeTemplateFragment> fragments = HalfInfiniteIntervalDetector.INSTANCE.detect(cube,"How many countries had amounts of more than 1000000 € in 2010?");
 		CubeTemplateFragment fragment = CubeTemplateFragment.combine(fragments);
-//		assertTrue(fragment.getRestrictions())
-//		System.out.println(HalfInfiniteIntervalDetector.INSTANCE.detect(cube,"extended amounts of > 1000000 and amount < 100"));
+		assertTrue(fragment.getRestrictions().size()==1);
+		Restriction restriction = fragment.getRestrictions().iterator().next();
+		assertEquals(restriction.getProperty().uri,"http://linkedspending.aksw.org/ontology/finland-aid-amount");
+		assertTrue(restriction.wherePatterns().size()==2);
+		assertTrue(restriction.wherePatterns().toString().contains("> \"1000000"));
 	}
-
 }
