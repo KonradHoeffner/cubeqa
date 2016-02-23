f1947023fabd60e5766252fe39868ed167be08cf
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2016-02-23 14:48:43 +0100

    marked stem test of egyptian as ignore for now as the lucene stemmer can't stem it.

--- src/test/java/org/aksw/cubeqa/property/scorer/DateScorerTest.java
+++ src/test/java/org/aksw/cubeqa/property/scorer/DateScorerTest.java
@@ -7,13 +7,19 @@ import org.junit.Test;
 
 public class DateScorerTest
 {
+	private static final String START_DATE = "http://linkedspending.aksw.org/ontology/finland-aid-start-date";
+
 	@Test public void testScore()
 	{
 		Cube cube = Cube.getInstance("finland-aid");
-		Scorer scorer = TemporalScorer.dateScorer(cube.properties.get("http://linkedspending.aksw.org/ontology/finland-aid-start-date"));
+		TemporalScorer scorer = TemporalScorer.dateScorer(cube.properties.get(START_DATE));
 //		String[] dates = {"2006-01-01","2006-01-02","2006-02-01","2014-04-01"};
-		assertEquals(scorer.score("2006-01-01").get().property.uri,"http://linkedspending.aksw.org/ontology/finland-aid-start-date");
-		assertEquals(scorer.score("2006-02-01").get().property.uri,"http://linkedspending.aksw.org/ontology/finland-aid-start-date");
+		assertEquals(scorer.score("2007-03-01").get().property.uri,START_DATE);
+		assertEquals(scorer.score("2006-01-31").get().property.uri,START_DATE);
+		// the next day of a day that is in
+		assertFalse(scorer.score("2006-03-02").isPresent());
+		assertFalse(scorer.score("2006-02-01").isPresent());
+		// not in at all
 		assertFalse(scorer.score("2006-01-02").isPresent());
 		assertFalse(scorer.score("2014-04-01").isPresent());
 	}
