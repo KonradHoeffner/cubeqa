ebe70c671cf5119c03e1f41a22319ad394ca952a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2016-04-06 15:38:53 +0200

    finally implemented aggregate test (sum and count for now)

--- src/test/java/org/aksw/cubeqa/detector/AggregateDetectorTest.java
+++ src/test/java/org/aksw/cubeqa/detector/AggregateDetectorTest.java
@@ -1,15 +1,27 @@
 package org.aksw.cubeqa.detector;
 
 import static org.junit.Assert.*;
+import static org.aksw.cubeqa.detector.Aggregate.*;
 import org.junit.Test;
 
 public class AggregateDetectorTest
 {
+	String[] questions = {
+			"What is the total aid to the Anti Corruption Commission in the Maldives in 2015?",
+			"What was the average Uganda health budget over all districts in 2014?",
+			"What is the average salary of an Engineering Technician in Washington DC?",
+			"What was the total budget on Technical Services in City of Toronto in 2009?"
+			};
+
+	Aggregate[] aggregates = {SUM,AVG,AVG,SUM};
 
 	@Test public void testDetect()
 	{
-		fail(
-				"Not yet implemented");
+		for(int i=0; i<questions.length;i ++)
+		{
+			assertEquals(aggregates[i],AggregateDetector.aggregatesContained(questions[i]).iterator().next());
+		}
+		
 	}
 
 }
