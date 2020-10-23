4ffac1b7f71963f1d5d9692289107c0a91ead6a8
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-10-23 16:12:50 +0200

    Update QALD links.

--- src/main/java/org/aksw/cubeqa/scripts/EvaluateQald6T3Test.java
+++ src/main/java/org/aksw/cubeqa/scripts/EvaluateQald6T3Test.java
@@ -4,8 +4,7 @@ import org.aksw.cubeqa.Algorithm;
 import org.aksw.cubeqa.benchmark.Benchmark;
 
 /** Evaluates the QALD 6 Task 3 test benchmark with 50 questions.
- * The test questions will be available at the following URI from April 8, 2016 and will then be checked into the repository as well.
- * @see <a href="http://greententacle.techfak.uni-bielefeld.de/~cunger/qald/index.php?x=challenge&q=6">http://greententacle.techfak.uni-bielefeld.de/~cunger/qald/index.php?x=challenge&q=6</a>. */
+ * @see <a href="https://qald.aksw.org/index.php?x=challenge&q=6">https://qald.aksw.org/index.php?x=challenge&q=6</a>. */
 public class EvaluateQald6T3Test
 {
 	public static void main(String[] args)
--- src/main/java/org/aksw/cubeqa/scripts/EvaluateQald6T3Train.java
+++ src/main/java/org/aksw/cubeqa/scripts/EvaluateQald6T3Train.java
@@ -4,7 +4,7 @@ import org.aksw.cubeqa.Algorithm;
 import org.aksw.cubeqa.benchmark.Benchmark;
 
 /** Evaluates the QALD 6 Task 3 train benchmark with 100 questions.
- * @see <a href="http://greententacle.techfak.uni-bielefeld.de/~cunger/qald/index.php?x=challenge&q=6">http://greententacle.techfak.uni-bielefeld.de/~cunger/qald/index.php?x=challenge&q=6</a>. */
+ * @see <a href="https://qald.aksw.org/index.php?x=challenge&q=6">https://qald.aksw.org/index.php?x=challenge&q=6</a>. */
 public class EvaluateQald6T3Train
 {
 	public static void main(String[] args)
